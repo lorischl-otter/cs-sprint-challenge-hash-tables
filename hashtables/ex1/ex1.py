19f6be0f6403ce708b9b0c967aa90ebe3fd3cb38
Lori Schlatter <lori.schlatter@gmail.com>
2020-08-07 10:47:32 -0500

    Completed ex1.py with all tests passing.

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,33 @@
 def get_indices_of_item_weights(weights, length, limit):
     """
-    YOUR CODE HERE
+    Takes a list of weights, length of list, and a weight limit.
+    Returns the 2 weights' indices that add to the limit,
+    in reverse order.
+
+    Returns None if no two are found.
     """
-    # Your code here
+    # Create a blank dictionary
+    d = {}
+
+    # Store values of weights in dict
+    # key = weight, value = index in list
+    for i, w in enumerate(weights):  # O(n)
+        # handle for duplicate weights
+        if w in d:
+            d[w] = (i, d[w])
+        else:
+            d[w] = i
+
+    # Check to see if limit - weight exists in dict
+    for k, v in d.items():  # O(n)
+        if limit - k in d:
+            # handle duplicate weights
+            if d[limit-k] == v:
+                return v
+            return (d[limit-k], v)
 
     return None
+
+# This passes the current tests, but would likely not pass
+# in the event of duplicate weights but a limit that only
+# requests one of the weights
