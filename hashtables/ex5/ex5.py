bececcafd4fe1935d75766219d0b9bff8d2aef9e
Lori Schlatter <lori.schlatter@gmail.com>
2020-08-07 12:32:56 -0500

    ex5.py passing 1 of 2 tests.

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -1,14 +1,30 @@
 # Your code here
 
 
-
 def finder(files, queries):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    # Create empty dictionary
+    d = {}
 
-    return result
+    # Iterate through files to store in dict
+    # key = file name, value = full file path
+    for f in files:  # O(n) over files
+        # Use "/" as split to get filename
+        filename = f.split("/")[-1]
+        d[filename] = f
+
+    # Generate empty list for results
+    results = []
+
+    # Iterate over queries to see which are present in dict
+    for q in queries:
+        # If query in dict, append filepath to results
+        if q in d:
+            results.append(d[q])
+
+    return results
 
 
 if __name__ == "__main__":
