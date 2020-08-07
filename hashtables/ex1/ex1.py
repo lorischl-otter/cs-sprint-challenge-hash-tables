56ed418485a489fd36a5286d22eff3ca6ec3d0f8
Lori Schlatter <lori.schlatter@gmail.com>
2020-08-07 12:50:43 -0500

    Updated ex1.py to cover more additional, untested use cases.

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -2,38 +2,36 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     Takes a list of weights, length of list, and a weight limit.
     Returns the 2 weights' indices that add to the limit,
-    in reverse order.
+    in reverse order. (higher index returned first)
 
     Returns None if no two are found.
 
-    Overall runtime is approximately O(2n)
+    Overall runtime is approximately O(n)
     """
     # Create a blank dictionary
     d = {}
 
-    # Store values of weights in dict
+    # Store values of weights in dict (in lists in case of duplicates)
     # key = weight, value = index in list
-    for i, w in enumerate(weights):  # O(n)
-        # handle for duplicate weights
-        if w in d:
-            d[w] = (i, d[w])
-        else:
-            d[w] = i
+    for i, w in enumerate(weights):  # O(n) over weights
+        # Initialize list if weight not already in dict
+        if w not in d:
+            d[w] = []
+        # Insert value at top of list
+        d[w].insert(0, i)
 
     # Check to see if limit - weight exists in dict
-    for k, v in d.items():  # O(n)
+    for k, v in d.items():  # O(n) over length of dict
         if limit - k in d:
-            # handle duplicate weights
+            # If limit and k values are the same
+            # Check for duplicate weights
             if d[limit-k] == v:
-                return v
-            return (d[limit-k], v)
+                # If only one index listed, then return None
+                if len(v) == 1:
+                    return None
+                # Else, return the first two indices of the list
+                return (v[0], v[1])
+            # Return the first index from each list
+            return (d[limit-k][0], v[0])
 
     return None
-
-# This passes the current tests, but would likely not pass
-# in the event of duplicate weights but a limit that only
-# requests one of the weights
-
-# Perhaps should initialize each value as an empty list,
-# then simply append the value each time added
-# Then when outputting, index the list for the correct values
